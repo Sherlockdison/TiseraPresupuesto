aea836cc57681766c89a1f34ed2ef0bbc970b6a1
Sherlockdison <dison.martinz@gmail.com>
2019-01-24 18:27:00 -0300

    maqueta sobre view presupuesto

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,42 +3,53 @@
 <html lang="es" dir="ltr">
 	<head>
 		<!-- meta -->
+
 		<meta charset="utf-8">
+
 			<!-- CSRF Token -->
+
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 		<!-- title -->
+
 		<title>@yield('title')</title>
 
 		<!-- scripts -->
+
 	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
 
 		<!-- Fonts -->
+
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 
 		<!-- styles -->
-		{{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> --}}
+
 		<link rel="stylesheet" href="{{ asset('/css/app.css') }}">
-		<link rel="stylesheet" href="{{ asset('/css/styles.css') }}" title="default">
-		{{-- <link rel="alternate stylesheet" href="{{ asset('/css/styles-dark.css') }}" title="dark"> --}}
+		<link rel="stylesheet" href="{{ asset('/css/styles.css') }}">
+
 		<!-- favicon -->
+
 		{{-- <link rel="shortcut icon" href="{{ asset('/public/favicon.jpg') }}"> --}}
 	</head>
 
 	<!-- body -->
+
 	<body class="@yield('name')" onload="set_style_from_cookie()">
+
 		<!-- header -->
+
 		@include('layouts.navbar')
 
 		<!-- content -->
-		{{-- <div class="container"> --}}
+
 			@yield('main_content')
-		{{-- </div> --}}
+
 
 		<!-- footer -->
+		
     {{-- @include('layout.footer') --}}
 
 	<!-- script -->
--- resources/views/presupuesto.blade.php
+++ resources/views/presupuesto.blade.php
@@ -4,5 +4,174 @@
 
 @section('main_content')
 
-<h2>HOLA WORLD</h2>
+  <div class="container">
+    <div class="col-md-12">
+      <div class="row">
+        <div class="col-md-2">
+          <h4>aca va tipo de operacion</h4>
+        </div>
+        <div class="col-md-8">
+          <h1>Presupuesto, estaria bueno que cambie segun eleccion</h1>
+        </div>
+        <div class="col-md-2">
+          <h4>aca va numeracion automatica</h4>
+        </div>
+      </div>
+
+
+      <div class="row">
+        <div class="col-md-6">
+
+          <h2>Logotipo y Datos de cada sucursal</h2>
+
+        </div>
+
+        <div class="col-md-6">
+          <div class="row">
+            <div class="col-md-6">
+              <h3>DATOS CLIENTE</h3>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-4">
+              <h4>Razon Social</h4>
+            </div>
+            <div class="col-md-8">
+              <h4>esto esta vacio</h4>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-4">
+              <h4>Domicilio Fiscal</h4>
+            </div>
+            <div class="col-md-8">
+              <h4>esto esta vacio</h4>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-4">
+              <h4>tELEFONO</h4>
+            </div>
+            <div class="col-md-8">
+              <h4>esto esta vacio</h4>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-4">
+              <h4>CUIT</h4>
+            </div>
+            <div class="col-md-8">
+              <h4>esto esta vacio</h4>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-4">
+              <h4>EMAIL</h4>
+            </div>
+            <div class="col-md-8">
+              <h4>esto esta vacio</h4>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+      <div class="row">
+        <div class="col-md-6">
+          <h3>fecha</h3>
+        </div>
+        <div class="col-md-6">
+          <h3>hora</h3>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-12">
+          {{-- quiero agregar campos y este div sirve para otra columna y mostrar segun--}}
+          <form role="form">
+            <div class="row">
+              <div class="col-md-2">
+                <label for="Codigo">Codigo:</label>
+                <input type="text" class="form-control" id="Codigo">
+              </div>
+              <div class="col-md-2">
+                <label for="Cantidad">Cantidad:</label>
+                <input type="number" class="form-control" id="Cantidad">
+              </div>
+              <div class="col-md-4">
+                <label for="Aclaraciones">Aclaraciones:</label>
+                <input type="text" class="form-control" id="Aclaraciones">
+              </div>
+              <div class="col-md-4">
+                <button type="submit" class="btn btn-default">Agregar</button>
+              </div>
+            </div>
+          </form>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-1" style="background-color:#aaa">
+        <P>CODIGO</P>
+      </div>
+      <div class="col-md-3" style="background-color:#bbb">
+        <P>DESCRIPCION</P>
+      </div>
+      <div class="col-md-1" style="background-color:#aaa">
+        <P>CANTIDAD</P>
+      </div>
+      <div class="col-md-2" style="background-color:#bbb">
+        <P>ACLARACIONES</P>
+      </div>
+      <div class="col-md-3" style="background-color:#aaa">
+        <P>FOTO</P>
+      </div>
+      <div class="col-md-1" style="background-color:#bbb">
+        <P>PRECIO/U</P>
+      </div>
+      <div class="col-md-1" style="background-color:#aaa">
+        <P>TOTAL PRODUCTO</P>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-1" style="background-color:#aaa">
+        <P>f52</P>
+      </div>
+      <div class="col-md-3" style="background-color:#bbb">
+        <P>sillon royal tatatatatat</P>
+      </div>
+      <div class="col-md-1" style="background-color:#aaa">
+        <P>2</P>
+      </div>
+      <div class="col-md-2" style="background-color:#bbb">
+        <P>todo en color marron oscurrito</P>
+      </div>
+      <div class="col-md-3" style="background-color:#aaa">
+        <P>Fotiqui</P>
+      </div>
+      <div class="col-md-1" style="background-color:#bbb">
+        <P>4400</P>
+      </div>
+      <div class="col-md-1" style="background-color:#aaa">
+        <P>8800</P>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <p>DETALLE DE ENTREGA</p>
+      </div>
+
+    </div>
+
+    {{-- div col-md-12 --}}
+  </div>
+  {{-- div container --}}
+</div>
+
 @endsection
