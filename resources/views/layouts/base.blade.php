b75b179560086cc78c12a42b1ac6ecd1735493ac
Sherlockdison <dison.martinz@gmail.com>
2019-01-24 13:45:17 -0300

    loyouts funcionales y font, creacion PresupuestosController@...

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,56 @@
+<!-- LAYOUT BLADE HTML CONFIG -->
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+	<head>
+		<!-- meta -->
+		<meta charset="utf-8">
+			<!-- CSRF Token -->
+		<meta name="csrf-token" content="{{ csrf_token() }}">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<!-- title -->
+		<title>@yield('title')</title>
+
+		<!-- scripts -->
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+		<!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
+		<!-- styles -->
+		{{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> --}}
+		<link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+		<link rel="stylesheet" href="{{ asset('/css/styles.css') }}" title="default">
+		{{-- <link rel="alternate stylesheet" href="{{ asset('/css/styles-dark.css') }}" title="dark"> --}}
+		<!-- favicon -->
+		{{-- <link rel="shortcut icon" href="{{ asset('/public/favicon.jpg') }}"> --}}
+	</head>
+
+	<!-- body -->
+	<body class="@yield('name')" onload="set_style_from_cookie()">
+		<!-- header -->
+		@include('layouts.navbar')
+
+		<!-- content -->
+		{{-- <div class="container"> --}}
+			@yield('main_content')
+		{{-- </div> --}}
+
+		<!-- footer -->
+    {{-- @include('layout.footer') --}}
+
+	<!-- script -->
+		      <script src="js/jquery-3.3.1.min.js"></script>
+					   <script>
+						     $(".toggle-nav").click(function () {
+							          $(".navbar-nav ul").slideToggle(350);
+		              });
+		        </script>
+						<script src="js/theme.js">
+
+						</script>
+		@yield ('page-scripts')
+	</body>
+</html>
---
+++ resources/views/presupuesto.blade.php
@@ -0,0 +1,8 @@
+@extends ('layouts.base')
+
+@section('title', 'Presupuesto')
+
+@section('main_content')
+
+<h2>HOLA WORLD</h2>
+@endsection
